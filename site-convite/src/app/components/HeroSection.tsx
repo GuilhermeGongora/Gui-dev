54e26baaa9c18874a0b3e0dd4b805db7e9c902ac
GuilhermeGongora <138256246+GuilhermeGongora@users.noreply.github.com>
2025-02-17 14:47:52 +0000

    Revert "New `Typewriter` feature, `colors` and `fonts`"
    This reverts commit 69d1aaf214df2d7b9d6a3c29b1ac33769741779b.

--- site-convite/src/app/components/HeroSection.tsx
+++ site-convite/src/app/components/HeroSection.tsx
@@ -1,49 +1,31 @@
 "use client";
 import { motion } from "framer-motion";
-import { Typewriter } from "react-simple-typewriter";
 
 export default function HeroSection() {
   return (
-    <div className="text-center py-10 bg-black ">
+    <div className="text-center py-20 bg-slate-950">
       <motion.h1
-        className="text-7xl font-bold text-[#00ff00] font-mono my-10"
+        className="text-6xl font-bold text-white"
         initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 10, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <Typewriter
-          words={["Console.log(Guilherme Gongora);"]}
-          loop={1}
-          cursor
-          cursorStyle="|"
-          typeSpeed={70}
-          deleteSpeed={50}
-          cursorColor="#00ff00" // Tailwind green-500
-        />
+        GE, EU TE AMO MUITO, AMOR!💖💕
       </motion.h1>
-      <motion.h2
-        className="text-4nx1 font-bold text-[#00ff00] py-5 font-mono"
+      <motion.p
+        className="text-2xl font-bold text-white py-10"
         initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 10, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <Typewriter
-          words={["Domine os mais diversos assuntos do mundo da tecnologia!"]}
-          loop={1}
-          cursor
-          cursorStyle="|"
-          typeSpeed={70}
-          deleteSpeed={50}
-          cursorColor="#22c55e"
-        />
-      </motion.h2>
-
+        Domine Git, JavaScript, Next.js, PostgreSQL e muito mais!
+      </motion.p>
       <motion.button
-        className="mt-4 bg-black text-[#00ff00] border-2 px-4 py-2 rounded-lg border-solid border-[#00ff00] shadow-md "
+        className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
-        Download CV
+        Quero me inscrever
       </motion.button>
     </div>
   );
