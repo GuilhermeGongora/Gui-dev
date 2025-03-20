12ffce7e6a26743fdefe1b00dc4fbf0193ff5908
GuilhermeGongora <138256246+GuilhermeGongora@users.noreply.github.com>
2025-03-20 00:58:14 +0000

    Typewriter removed from `HeroSection`

--- site-convite/src/app/components/HeroSection.tsx
+++ site-convite/src/app/components/HeroSection.tsx
@@ -38,22 +38,6 @@ export default function HeroSection() {
           cursorColor="#00ff00"
         />
       </motion.h5>
-      <motion.h2
-        className="text-xl md:text-3xl lg:text-4xl font-bold text-[#00ff00] py-5 font-mono"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >
-        <Typewriter
-          words={["Domine os mais diversos assuntos do mundo da tecnologia!"]}
-          loop={1}
-          cursor
-          cursorStyle="|"
-          typeSpeed={70}
-          deleteSpeed={50}
-          cursorColor="#22c55e"
-        />
-      </motion.h2>
 
       <div className="mt-6 w-full flex justify-center">
         <motion.a
